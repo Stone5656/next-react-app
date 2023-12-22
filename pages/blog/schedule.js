a3aea4a469ecccd16fd32ee153ef240191cb3815
Stone5656 <131738157+Stone5656@users.noreply.github.com>
2023-12-22 16:30:43 +0900

    12/22

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,15 +1,58 @@
-import { client } from "lib/api"
+import { getPostBySlug } from "lib/api"
+import Container from 'components/container'
+import PostHeader from "@/components/post-header"
+import Image from "next/image"
+import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from "@/components/two-column"
 
-export default function Schedule() {
-  return <h1>記事のタイトル</h1>
+import Postbody from "@/components/post-body"
+import ConvertBody from "@/components/convert-body"
+import PostCategories from "@/components/post-categories"
+
+export default function Schedule({
+  title,
+  publish,
+  content,
+  eyecatch,
+  categories,
+}) {
+  return (
+    <Container>
+      <article>
+        <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+        <figure>
+          <Image
+          src={eyecatch.url}
+          alt=""
+          layout="responsive"
+          width={eyecatch.width}
+          height={eyecatch.height}
+          sizes="(min-width: 1152px) 1152px, 100vw"
+          priority
+          />
+        </figure>
+        <TwoColumn>
+          <TwoColumnMain>
+            <Postbody><ConvertBody contentHTML={content} /></Postbody>
+          </TwoColumnMain>
+          <TwoColumnSidebar>
+            <PostCategories categories={categories} />
+          </TwoColumnSidebar>
+        </TwoColumn>
+      </article>
+    </Container>
+  )
 }
 export async function getStaticProps() {
-  const resPromise = client.get({
-    endpoint: 'blogs',
-  })
-  resPromise.then((res) => console.log(res)).catch((err) => console.log(err))
+  const slug = 'schedule'
 
+  const post = await getPostBySlug(slug)
   return {
-    props: {},
+    props: {
+      title: post.title,
+      publishDate: post.publishDate,
+      content: post.content,
+      eyecatch: post.eyecatch,
+      categories: post.categories,
+    },
   }
 }
